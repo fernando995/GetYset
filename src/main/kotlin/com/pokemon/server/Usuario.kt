54500d13a66b1ed35c996d0a2ab2af057ed9e4c7
Fernando <fertru245@alu.centroafuera.es>
2022-05-23 12:47:47 +0200

    Server Pokemon

--- src/main/kotlin/com/pokemon/server/Usuario.kt
+++ src/main/kotlin/com/pokemon/server/Usuario.kt
@@ -1,5 +1,6 @@
 package com.pokemon.server
 
+import javax.persistence.ElementCollection
 import javax.persistence.Entity
 import javax.persistence.Id
 
@@ -7,4 +8,6 @@ import javax.persistence.Id
 data class Usuario(@Id var nombre: String, var pass: String) {
     val token = nombre + pass
     var pokemonFavoritoId : Int? = null
+    @ElementCollection
+    var pokemonCapturado  = mutableListOf<Int>()
 }
