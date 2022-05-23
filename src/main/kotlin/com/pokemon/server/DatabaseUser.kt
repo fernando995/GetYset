f4676832f9282b599da08fe7f58c3f9dd0e2c5ef
Fernando <fertru245@alu.centroafuera.es>
2022-05-23 12:47:47 +0200

    Server Pokemon

---
+++ src/main/kotlin/com/pokemon/server/DatabaseUser.kt
@@ -0,0 +1,8 @@
+package com.pokemon.server
+
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class DatabaseUser {
+
+}
--- src/main/kotlin/com/pokemon/server/UsuarioController.kt
+++ src/main/kotlin/com/pokemon/server/UsuarioController.kt
@@ -8,7 +8,7 @@ class UsuarioController(private val usuarioRepository: UsuarioRepository) {
     // Podemos hacer la request desde el navegador.
     @GetMapping("crearUsuario/{nombre}/{pass}")
     @Synchronized
-    fun requestCrearUsuario(@PathVariable nombre: String, @PathVariable pass: String) : Any {
+    fun requestCrearUsuario(@PathVariable nombre: String, @PathVariable pass: String): Any {
         val userOptinal = usuarioRepository.findById(nombre)
 
         return if (userOptinal.isPresent) {
@@ -30,7 +30,7 @@ class UsuarioController(private val usuarioRepository: UsuarioRepository) {
      */
     @PostMapping("crearUsuario")
     @Synchronized
-    fun requestCrearUsuarioJson(@RequestBody usuario: Usuario) : Any {
+    fun requestCrearUsuarioJson(@RequestBody usuario: Usuario): Any {
         val userOptinal = usuarioRepository.findById(usuario.nombre)
 
         return if (userOptinal.isPresent) {
@@ -47,17 +47,73 @@ class UsuarioController(private val usuarioRepository: UsuarioRepository) {
     }
 
 
-    @PostMapping("pokemonFavorito/{token}/{pokemonId}")
-    fun guardarPokemonFavorito(@PathVariable token: String, @PathVariable pokemonId: Int) : Any {
+    @PostMapping("pokemonFavorito/{token}")
+    fun guardarPokemonFavorito(@PathVariable token: String): Any {
         println(token)
         usuarioRepository.findAll().forEach { user ->
             if (user.token == token) {
-                user.pokemonFavoritoId = pokemonId
-                usuarioRepository.save(user)
-                return "El usuario ${user.nombre} tiene un nuevo Pokémon favorito"
-            }
+                listaPokemon.listaPokemon.forEach {
+                    if (user.pokemonFavoritoId?.toLong() == it.id) {
+                        return it
+                    }
+                }
+                return "el usuario no tiene pokemon favorito"
+            } else
+                return "Token No  valido"
         }
         return "Token no encontrado"
     }
 
+
+    @PostMapping("pokemonCapturado/{token}/{pokemonId}")
+    fun pokemonCapturado(@PathVariable token: String, @PathVariable pokemonId: Int): String {
+
+        usuarioRepository.findAll().forEach { user ->
+
+            if (user.token == token) {
+                user.pokemonCapturado.add(pokemonId)
+                usuarioRepository.save(user)
+                return "El usuario ${user.nombre} tiene ${user.pokemonCapturado} capturados"
+
+            }
+        }
+
+        return "token no encontrado"
+
+    }
+
+
+    @GetMapping("mostrarPokemonCapturados/{token}")
+    fun requestMostrarPokemonCapturados(@PathVariable token: String): Any {
+
+        var listacapturados = mutableListOf<String>()
+
+        usuarioRepository.findAll().forEach { user ->
+
+            if (user.token == token) {
+
+                user.pokemonCapturado.forEach { idPokemon ->
+
+                    println(idPokemon)
+                    listaPokemon.listaPokemon.forEach {pokemon ->
+                        if (idPokemon.toLong() == pokemon.id) {
+                            println(pokemon.name)
+                            listacapturados.add(pokemon.name)
+                        }
+
+                    }
+
+                }
+
+            }
+
+        }
+
+        return if (listacapturados.isEmpty())
+            "El usuario no ha capturado"
+        else
+            listacapturados
+
+
+    }
 }
